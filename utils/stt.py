6b72c196ff1879cc046c5bf4dcfac4ae10c1f8bc
nikita2026 <nikitabaxi1102@gmail.com>
2025-10-28 21:41:06 -0700

    Updated STT credentials - API Key & URL

--- utils/stt.py
+++ utils/stt.py
@@ -3,9 +3,9 @@ from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 def transcribe_audio(audio_file, language):
-    authenticator = IAMAuthenticator('YOUR_IBM_API_KEY')
+    authenticator = IAMAuthenticator('nohuGThh7Hi9xknxWyWXaxnL1-0QysaxVCnnP5mefGtb')
     stt = SpeechToTextV1(authenticator=authenticator)
-    stt.set_service_url('YOUR_IBM_STT_URL')
+    stt.set_service_url('https://api.au-syd.speech-to-text.watson.cloud.ibm.com/instances/5ee0282b-2c4f-4d5b-990d-b5047a2186c9')
 
     response = stt.recognize(audio=audio_file, content_type='audio/wav', model=f'{language}_BroadbandModel').get_result()
     return response['results'][0]['alternatives'][0]['transcript']
