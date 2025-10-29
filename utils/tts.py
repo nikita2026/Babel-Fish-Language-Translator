71e46866c4f81ce0f265f4e1eff222c28363c77f
nikita2026 <nikitabaxi1102@gmail.com>
2025-10-28 21:41:40 -0700

    Updated TTS credentials - API Key & URL

--- utils/tts.py
+++ utils/tts.py
@@ -3,9 +3,9 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import base64
 
 def synthesize_speech(text, language):
-    authenticator = IAMAuthenticator('YOUR_IBM_API_KEY')
+    authenticator = IAMAuthenticator('rGSdEsOW5CaVFrie7LVctvpoY4xnT7PcvnHbJrS7vR3S')
     tts = TextToSpeechV1(authenticator=authenticator)
-    tts.set_service_url('YOUR_IBM_TTS_URL')
+    tts.set_service_url('https://api.au-syd.text-to-speech.watson.cloud.ibm.com/instances/60cde0ad-6a7f-4c23-9373-cb4cbc07eb37')
 
     response = tts.synthesize(text, voice=f'{language}_Voice', accept='audio/wav').get_result().content
     return base64.b64encode(response).decode('utf-8')
